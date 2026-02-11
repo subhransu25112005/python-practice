662170588bdfa47062f451a2c113b35a5b77c1c6
Subhranshu Nanda <subhransu25112005@gmail.com>
2026-02-11 23:59:48 +0530

    working on solid principle

---
+++ Advance_Python.py/Oops_Depth/SolidPriciple.py
@@ -0,0 +1,7 @@
+'''🔠 S O L I D
+Letter	Principle
+S	Single Responsibility Principle
+O	Open/Closed Principle
+L	Liskov Substitution Principle
+I	Interface Segregation Principle
+D	Dependency Inversion Principle'''
